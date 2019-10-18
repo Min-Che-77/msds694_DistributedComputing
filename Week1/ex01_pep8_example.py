f182f4c68a79848d486a8c8ec01e83bbce84d8c5
Diane Woodbridge <dwoodbridge@usfca.edu>
2019-10-18 09:35:34 -0700

    added week1

---
+++ Week1/ex01_pep8_example.py
@@ -0,0 +1,13 @@
+import sys
+import string
+
+
+class Human:
+    """This class is defining human."""
+
+    def eat(self):
+        print("munch")
+
+
+jackson = Human()  # object of Human class
+jackson.eat()
